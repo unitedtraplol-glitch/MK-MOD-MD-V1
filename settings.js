dc502ca87d966b01a5ef9372a0decb490f3fa3dd
unitedtraplol-glitch <unitedtraplol@gmail.com>
2026-04-07 20:37:07 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'mkmodz313',
+  author: '‎',
+  botName: "MKMODZbot",
+  botOwner: 'mkmodz', // Your name
+  ownerNumber: '923327011312', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
